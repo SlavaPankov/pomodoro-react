d1904989fa0407dd3c1e362b4193130bfa722eb1
Slava <slavapank94@yandex.ru>
2023-07-12 21:51:07 +0600

    feat: implement total task time calculation

--- src/shared/TaskList/TaskList.tsx
+++ src/shared/TaskList/TaskList.tsx
@@ -1,10 +1,44 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import styles from './taskList.scss';
 import { Task } from './TaskItem';
 import { useAppSelector } from '../../store/hooks/hooks';
 
 export function TaskList() {
   const { tasks } = useAppSelector((state) => state.tasks);
+  const [totalTime, setTotalTime] = useState('');
+
+  function getNoun(number: number, one: string, two: string, five: string) {
+    let n = Math.abs(number);
+    n %= 100;
+    if (n >= 5 && n <= 20) {
+      return five;
+    }
+    n %= 10;
+    if (n === 1) {
+      return one;
+    }
+    if (n >= 2 && n <= 4) {
+      return two;
+    }
+    return five;
+  }
+
+  function convertTime(time: number) {
+    const hours = (time / 60);
+    const roundHours = Math.floor(hours);
+    const minutes = (hours - roundHours) * 60;
+    const roundMinutes = Math.round(minutes);
+    return `${roundHours} ${getNoun(roundHours, 'час', 'часа', 'часов')} и ${roundMinutes} ${getNoun(roundMinutes, 'минута', 'минуты', 'минут')}`;
+  }
+
+  useEffect(() => {
+    if (tasks.length === 0) {
+      return;
+    }
+
+    const tasksTime = tasks.reduce((acc, val) => acc + val.pomodoro * 25, 0);
+    setTotalTime(convertTime(tasksTime));
+  }, [tasks]);
 
   return (
     <ul className={`${styles.list} list-reset`}>
@@ -17,6 +51,7 @@ export function TaskList() {
             title={task.title}
           />)
       }
+      {totalTime && <li className={styles.totalTime}>{totalTime}</li>}
     </ul>
   );
 }
